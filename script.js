152c71c6e2dad2c2d43877204fc40df44a62a5ac
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2025-01-18 18:33:19 +0100

    changed a function name

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let flippedCards=[];
 let flipCount=0;
 const flipCounterElement=document.getElementById('flipCounter');
 
-function countFlips() {
+function incrementFlipCounter() {
   flipCount++;
   flipCounterElement.textContent = `Moves: ${flipCount}`;
 }
@@ -104,7 +104,7 @@ card.addEventListener('click', () => {
   }
   
 // Update flip count 
-countFlips();
+incrementFlipCounter();
 
   //Just 2 cards are flipped
 if (flippedCards.length === 2) {
